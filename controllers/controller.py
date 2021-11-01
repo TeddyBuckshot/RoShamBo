6b6a385638a57ee733c7f4e63bfcb69f9048dc71
Sam <samspencer2202@outlook.com>
2021-11-01 03:55:36 +0000

    extentions DONE

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,10 @@
 from flask import render_template, request
 from app import app
-from models import player
+from models import computer
 from models.player import Player
 from models import game
-
+from models.computer import Cmptr
+from models.game import Game
 
 @app.route('/home')
 def index():
@@ -13,7 +14,11 @@ def index():
 def start():
     return render_template('start.html', title='Start Game')
 
-@app.route('/result', methods=['POST'])
+@app.route("/computer")
+def play_computer():
+    return render_template('computer.html', title='Play the Computer')
+
+@app.route('/result1', methods=['POST'])
 def get_result():
     print(request.form)
     player_one_name = request.form['name1']
@@ -27,3 +32,15 @@ def get_result():
 
     return render_template('result.html', player_name_1=new_player1.name, player_name_2=new_player2.name, player_one=player_one_choice, player_two=player_two_choice, winner=winner)
 
+@app.route('/result2', methods=['POST'])
+def get_comp_result():
+    player_name = request.form['name1']
+    player_choice = request.form['choice1']
+    new_player1 = Player(player_name, player_choice)
+    computer1 = Cmptr("Computer", computer.computer_choice())
+    computer_name = computer1.name
+    computer_choice = computer1.choice
+    new_game = Game(new_player1.name, computer_name, new_player1.choice, computer_choice)
+    winner = game.play_game(new_game.p1_name, new_game.p2_name, new_game.p1_choice, new_game.p2_choice)
+
+    return render_template('result.html', player_name1=new_player1.name, player_name_2=computer_name, player_one=new_player1.choice, player_two=computer_choice, winner=winner)
--- models/game.py
+++ models/game.py
@@ -1,3 +1,9 @@
+class Game():
+    def __init__(self, p1_name, p2_name, p1_choice, p2_choice):
+        self.p1_name = p1_name
+        self.p2_name = p2_name
+        self.p1_choice = p1_choice
+        self.p2_choice = p2_choice
 
 def play_game(name1, name2, choice1, choice2):
     winner = name2
@@ -9,5 +15,4 @@ def play_game(name1, name2, choice1, choice2):
         winner = name1
     if choice1 == "paper" and choice2 == "rock":
         winner = name1
-    
     return winner
