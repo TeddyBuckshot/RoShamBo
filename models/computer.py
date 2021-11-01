b84032dda4d095b409877f378191540e07059af4
Sam <samspencer2202@outlook.com>
2021-11-01 02:52:46 +0000

    added constructor

--- models/computer.py
+++ models/computer.py
@@ -1,5 +1,10 @@
 import random
 
+class Cmptr():
+    def __init__(self, name, choice):
+        self.name = name
+        self.choice = choice
+
 def computer_choice():
     choices = ['rock', 'paper', 'scissors']
     return choices[random.randint(0,2)]
